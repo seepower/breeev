51e7d81aa0b1ed62e515aa8d72051f2fc533a177
seepower <ionic@gmx.tm>
2017-04-17 17:21:45 +0200

    test4
    Test4

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,36 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 
-  var browser = cordova.InAppBrowser.open('https://www.breeev.me','_system','location=no,zoom=no,hidden=yes,toolbar=no');
-  
-  browser.addEventListener("loadstop", function(){
-    browser.show();
-  });
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+
+    // Mock device.platform property if not available
+    if (!window.device) {
+        window.device = { platform: 'Browser' };
+    }
+
+    handleExternalURLs();
+}
+
+function handleExternalURLs() {
+    // Handle click events for all external URLs
+    if (device.platform.toUpperCase() === 'ANDROID') {
+        $(document).on('click', 'a[href^="http"]', function (e) {
+            var url = $(this).attr('href');
+            navigator.app.loadUrl(url, { openExternal: true });
+            e.preventDefault();
+        });
+    }
+    else if (device.platform.toUpperCase() === 'IOS') {
+        $(document).on('click', 'a[href^="http"]', function (e) {
+            var url = $(this).attr('href');
+            window.open(url, '_system');
+            e.preventDefault();
+        });
+    }
+    else {
+        // Leave standard behaviour
+    }
+}
 
