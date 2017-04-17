5984d31798b665b3da9f3d939aa75d1c1bef3f94
seepower <ionic@gmx.tm>
2017-04-17 16:05:48 +0200

    test2
    test2

--- www/js/app.js
+++ www/js/app.js
@@ -3,10 +3,10 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-document.addEventListener("deviceready", function(){
+
   var browser = cordova.InAppBrowser.open('https://www.breeev.me','_system','location=no,zoom=no,hidden=yes,toolbar=no');
   
   browser.addEventListener("loadstop", function(){
     browser.show();
   });
-}, false);
+
